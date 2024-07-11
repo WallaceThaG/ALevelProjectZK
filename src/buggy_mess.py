f1a5ab8297cb44b251d7a0550bc682b99bb523be
WallaceThaG <123844990+WallaceThaG@users.noreply.github.com>
2024-07-11 13:01:00 +0100

    still working on DDA

--- src/buggy_mess.py
+++ src/buggy_mess.py
@@ -17,66 +17,142 @@ from pygame.locals import *
 class Player:
     # constructor method for the player
     def __init__(self):
-        # for now just initialise player position to the centre of the screen
-        self.angle = math.pi # important reminder here
-        self.position = pygame.math.Vector2(WIDTH/2,HEIGHT/2)
-        self.velocity = pygame.math.Vector2(0,0)
-        self.acceleration = pygame.math.Vector2(0,0)
-        self.friction = -0.15
+        self.x = WIDTH/2
+        self.y = HEIGHT/2
+        self.angle = PI/2
+        self.y_velocity = 0
+        self.x_velocity = 0
+        self.max_vel = 5
 
-    def update(self,dt):
-        self.move(dt)
-
-    def move(self,dt):
+    def update(self):
         keys = pygame.key.get_pressed()
+        self.move(keys)
+        self.rotate(keys)
+
+    def move(self, keys):
         # vertical movement
-        self.acceleration.y = 0
         if keys[K_w]:
-            self.acceleration.y += 0.2
+            self.x_velocity -= 0.3*math.cos(player.angle)
+            self.y_velocity -= 0.3*math.sin(player.angle)
         elif keys[K_s]:
-            self.acceleration.y += 0.2
-        self.acceleration.y -= self.velocity.y * self.friction
-        self.velocity.y -= self.acceleration.y * dt
-        self.position.y += self.velocity.y * dt + (0.5 * self.acceleration.y) * (dt * dt)
+            self.x_velocity += 0.3*math.cos(player.angle)
+            self.y_velocity += 0.3*math.sin(player.angle)
+        # deceleration
+        elif not(keys[K_a] or keys[K_d]):
+            self.x_velocity *= 0.95
+            self.y_velocity *= 0.95
         # horizontal movement
         if keys[K_a]:
-            self.acceleration.x += 0.2
+            self.x_velocity -= 0.3*math.sin(player.angle)
+            self.y_velocity += 0.3*math.cos(player.angle)
         elif keys[K_d]:
-            self.acceleration.x -= 0.2
-        self.acceleration.x += self.velocity.x * self.friction
-        self.velocity.x -= self.acceleration.x * dt
-        self.position.x += self.velocity.x * dt + (0.5 * self.acceleration.x) * (dt * dt)
-
-
-# global stuff here
-WIDTH  = 800
-HEIGHT = 600
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-TARGET_FPS = 60
-player = Player()
+            self.x_velocity += 0.3*math.sin(player.angle)
+            self.y_velocity -= 0.3*math.cos(player.angle)
+        # deceleration
+        elif not(keys[K_w] or keys[K_s]):
+            self.x_velocity *= 0.95
+            self.y_velocity *= 0.95
         
+        # limit velocity
+        self.x_velocity *= 0.96
+        self.y_velocity *= 0.96
+        # update coordinates
+        self.y -= self.y_velocity
+        self.x -= self.x_velocity
+
+    def rotate(self, keys):
+        # decrease player angle when left key is pressed
+        # increase when right key is pressed
+        if keys[K_LEFT]:
+            self.angle -= 0.1
+            # the player's angle must always be between 0 and 2*pi radians
+            if self.angle < 0:
+                self.angle += 2*PI - 0.00001  # add small value because it makes things less awkward later
+        elif keys[K_RIGHT]:
+            self.angle += 0.1
+            if self.angle > 2*PI:
+                self.angle -= 2*PI + 0.00001
+
+    def draw(self, line_start):
+        line_end = (int(self.x+15*math.cos(self.angle)), int(self.y+15*math.sin(self.angle)))
+        pygame.draw.line(WINDOW, (255,0,0), line_start, line_end, 3)
+        pygame.draw.circle(WINDOW, (255,0,0), (int(self.x), int(self.y)), 5)
+
 def draw():
     # fill window with the darkness
     WINDOW.fill((0,0,0))
-    pygame.draw.rect(WINDOW, (255,0,0), [int(player.position.x-5), int(player.position.y-5), 10, 10])
-    #pygame.draw.line(WINDOW, (255,0,0), ()) i want to draw the player as an arrow so their direction is clear
-    pygame.display.flip() 
+    player_pos = (int(player.x), int(player.y))
+    draw_map()
+    cast_rays(player_pos)
+    player.draw(player_pos)
+    pygame.display.flip()
+
+def draw_map():
+    for i in range(len(map_1)):
+        for j in range(len(map_1)):
+            if map_1[i][j] == "#":
+                # draw cells
+                pygame.draw.rect(WINDOW, (0,0,255), pygame.Rect(j*CELL_LENGTH, i*CELL_LENGTH, CELL_LENGTH, CELL_LENGTH))
+
+def cast_rays(ray_start):
+    ray_end = (int(player.x+512*math.cos(player.angle)), int(player.y+512*math.sin(player.angle)))
+    #ray_end = check_horizontal()
+    pygame.draw.line(WINDOW, (255, 255, 255), ray_start, ray_end)
+
+def check_horizontal():
+    player_map_x = int(player.x/64)
+    player_map_y = int(player.y/64)
+    if player.angle < PI: # player facing down
+        dy = int(player.y - (player_map_y + 1)*64)
+    else:
+        dy = int(player.y - player_map_y*64)
+    dx = int(dy/(math.tan(player.angle)))
+    if map_1[int((player.x+dx)/64) % 12][int((player.y+dy)/64) % 12] == "#":
+        return (int(player.x-dx), int(player.y-dy))
+    
+
+# global stuff here
+WIDTH = 1024
+HEIGHT = 768
+CELL_LENGTH = 64
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+PI = math.pi
+RAY_ANGLE = PI/360
+player = Player()
+# maps are 12x12 squares with square cells
+map_1 = [  "############",
+           "#     #    #",
+           "#     #    #",
+           "#   ####   #",
+           "#          #",
+           "#          #",
+           "#######    #",
+           "#     #    #",
+           "#     #    #",
+           "#          #",
+           "#          #",
+           "############",  ]
 
 def main():
+
+    global player
+
+    pygame.init()
     
     clock = pygame.time.Clock()
     running = True
 
     # main game loop
     while running:
-        dt = clock.tick(60)*0.001*TARGET_FPS
-        clock.tick(60)
+        clock.tick(60) # targeting 60 fps
+        pygame.display.set_caption(str(int(clock.get_fps()))) # keep track of fps
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
                 sys.exit()
-        player.update(dt)
+        # update attributes and draw to the screen
+        player.update()
         draw()
 
 main()
